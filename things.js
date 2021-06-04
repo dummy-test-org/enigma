e08cbf22d849ac8e6309bf97d955034cbc13c78d
Jay <jeremy.davis@sonarsource.com>
2021-06-04 11:29:50 +0200

    Update things.js

--- things.js
+++ things.js
@@ -5,3 +5,17 @@ const a = "hello" //FIXME use single quotes
 console.log('a = ' + a + " <-- cool!")
 
 //FIXME arrg
+for (let i = 0; i < 4345; i++) {
+  const t = i + 2
+  for (let j = 0; j < t; j++) {
+    if (a < j) {
+      console.log('a');
+    } else {
+      console.log('b');
+    }
+    
+    const toto = 'hello';
+  }
+}
+
+console.log(i);
